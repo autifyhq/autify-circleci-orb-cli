097875b41360d264d1eb91549b4a9a81dc586c49
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-06-28 14:10:12 +0900

    Implement install.bash

--- src/scripts/install.bash
+++ src/scripts/install.bash
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo Hello "${PARAM_VERSION}"
+curl https://autify-cli-assets.s3.amazonaws.com/autify-cli/channels/stable/install-standalone.sh | bash
+
+if [ "$PARAM_VERSION" != "stable" ]; then
+  autify update -v $PARAM_VERSION
+fi
