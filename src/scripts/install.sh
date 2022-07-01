89e70fce9d6724a70d9000fc5c19002914ab01b2
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-07-01 15:51:38 +0900

    Fix shellcheck violation
    Fixing the violation of SC2086.
    
    Also, adjusting to run shellcheck properly in the workflow.

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -2,5 +2,5 @@
 curl https://autify-cli-assets.s3.amazonaws.com/autify-cli/channels/stable/install-standalone.sh | bash
 
 if [ "$PARAM_VERSION" != "stable" ]; then
-  autify update -v $PARAM_VERSION
+  autify update -v "$PARAM_VERSION"
 fi
